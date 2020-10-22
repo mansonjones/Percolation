8bd0d17959f4313cdebb66dd89157551b1ea3bae
Manson Jones <manson.jones@gmail.com>
2020-10-22 11:52:40 -0700

    Initial Commit

---
+++ Percolation/PercolationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PercolationApp.swift
+//  Percolation
+//
+//  Created by Manson Jones on 10/22/20.
+//
+
+import SwiftUI
+
+@main
+struct PercolationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
